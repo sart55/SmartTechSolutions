5de30283718e37f57eebd5f5c66123066f1ae47b
sart55 <105958020+sart55@users.noreply.github.com>
2025-10-06 19:42:37 +0530

    Update AdminPage.jsx

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -168,7 +168,7 @@ const handleExcelUpload = (e) => {
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
       const rows = XLSX.utils.sheet_to_json(sheet);
-      console.log("Parsed Excel data:", rows);
+      //console.log("Parsed Excel data:", rows);
 
       const nowIso = new Date().toISOString();
 
@@ -796,6 +796,7 @@ const saveComponents = async () => {
 }
 
 export default AdminPage;
+
 
 
 
